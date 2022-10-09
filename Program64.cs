5493ac000f64f392848ccdf7e906ed36ffcd8069
Denis76842 <112509983+Denis76842@users.noreply.github.com>
2022-10-09 14:20:30 +0300

    Add files via upload

---
+++ Program64.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+int M = 1;
+Console.WriteLine();
+if (N>M)
+for (int i = N; i >= 1; i--)
+    Console.Write($" {i},   ");
+else
+    for (int i = M; i >= N; i--)
+        Console.Write($" {i},   ");
